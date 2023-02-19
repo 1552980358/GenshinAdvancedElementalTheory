ff426b860b0949578b2d898c39266b15d69a6f4c
1552980358 <1552980358@qq.com>
2023-02-20 05:37:29 +0800

    [base/BaseFragment] Apply methods from FragmentLifecycleUtil

--- app/src/main/java/projekt/cloud/piece/genshin/elements/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/genshin/elements/base/BaseFragment.kt
@@ -6,33 +6,25 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import projekt.cloud.piece.genshin.elements.util.FragmentLifecycleUtil.clearOnViewDestroy
+import projekt.cloud.piece.genshin.elements.util.FragmentLifecycleUtil.closeOnViewDestroy
 import projekt.cloud.piece.genshin.elements.util.ViewBindingReflector.LayoutBindingReflectorUtil.inflate
 
 abstract class BaseFragment<out VB: ViewBinding, out LC: BaseLayoutCompat<VB>>(
     private val viewBindingClass: Class<VB>
 ): Fragment() {
 
-    private var _binding: VB? = null
-    protected val binding: VB
-        get() = _binding!!
+    protected var binding: @UnsafeVariance VB by clearOnViewDestroy()
+        private set
 
-    private var _layoutCompat: @UnsafeVariance LC? = null
-    protected val layoutCompat: LC
-        get() = _layoutCompat!!
+    private var _layoutCompat: @UnsafeVariance LC by closeOnViewDestroy()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = viewBindingClass.inflate(inflater, container)
+        binding = viewBindingClass.inflate(inflater, container)
         _layoutCompat = onCreateLayoutCompat(binding)
         return binding.root
     }
 
     protected abstract fun onCreateLayoutCompat(binding: @UnsafeVariance VB): LC
-
-    override fun onDestroyView() {
-        layoutCompat.destroy()
-        _layoutCompat = null
-        _binding = null
-        super.onDestroyView()
-    }
 
 }
