1898cdbf15b4b6a117d85c99fe5d2eddd6fde942
1552980358 <1552980358@qq.com>
2023-02-19 17:05:36 +0800

    [base/BaseFragment] Update BaseLayoutCompat and destroy

--- app/src/main/java/projekt/cloud/piece/genshin/elements/base/BaseFragment.kt
+++ app/src/main/java/projekt/cloud/piece/genshin/elements/base/BaseFragment.kt
@@ -8,15 +8,31 @@ import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
 import projekt.cloud.piece.genshin.elements.util.ViewBindingReflector.LayoutBindingReflectorUtil.inflate
 
-abstract class BaseFragment<VB: ViewBinding>(private val viewBindingClass: Class<VB>): Fragment() {
+abstract class BaseFragment<out VB: ViewBinding, out LC: BaseLayoutCompat<VB>>(
+    private val viewBindingClass: Class<VB>
+): Fragment() {
 
     private var _binding: VB? = null
     protected val binding: VB
         get() = _binding!!
 
+    private var _layoutCompat: @UnsafeVariance LC? = null
+    protected val layoutCompat: LC
+        get() = _layoutCompat!!
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = viewBindingClass.inflate(inflater, container)
+        _layoutCompat = onCreateLayoutCompat(binding)
         return binding.root
+    }
+
+    protected abstract fun onCreateLayoutCompat(binding: @UnsafeVariance VB): LC
+
+    override fun onDestroyView() {
+        layoutCompat.destroy()
+        _layoutCompat = null
+        _binding = null
+        super.onDestroyView()
     }
 
 }
